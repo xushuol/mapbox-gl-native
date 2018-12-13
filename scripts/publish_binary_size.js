a6f9016765a1b99221d814b7defa60c8597b710b
Nadia Barbosa <nadiabarbosa@me.com>
2018-12-13 14:59:41 -0800

    Revert to original publish_binary_size.js

--- scripts/publish_binary_size.js
+++ scripts/publish_binary_size.js
@@ -39,9 +39,7 @@ const platforms = [
     { 'platform': 'Android', 'arch': 'x86_64' }
 ];
 
-const date = new Date();
 const rows = [];
-var metricsPayload = [];
 
 function query(after) {
     return github.request({
@@ -87,10 +85,6 @@ function query(after) {
             }`
     }).then((result) => {
         const history = result.data.data.repository.ref.target.history;
-        console.log("---------Dumping history-------");
-        console.log(JSON.stringify(history))
-        console.log("-------------------------------");
-        const lastCommitSizeChecks = history.edges[0].node.checkSuites.nodes[0].checkRuns.nodes;
 
         for (const edge of history.edges) {
             const commit = edge.node;
@@ -99,97 +93,37 @@ function query(after) {
             if (!suite)
                 continue;
 
-            // Build source data for http://mapbox.github.io/mapbox-gl-native/metrics/binary-size/
-            createChartSourceData(commit);
+            const runs = commit.checkSuites.nodes[0].checkRuns.nodes;
+            const row = [`${commit.oid.slice(0, 7)} - ${commit.messageHeadline}`];
+
+            for (let i = 0; i < platforms.length; i++) {
+                const {platform, arch} = platforms[i];
+
+                const run = runs.find((run) => {
+                    const [, p, a] = run.name.match(/Size - (\w+) ([\w-]+)/);
+                    return platform === p && arch === a;
+                });
+
+                row[i + 1] = run ? +run.summary.match(/is (\d+) bytes/)[1] : undefined;
+            }
+
+            rows.push(row);
         }
 
         if (history.pageInfo.hasNextPage) {
             return query(history.pageInfo.endCursor);
         } else {
-          
-          // Send result of binary size checks of last commit to S3
-          const binaryMetrics = [];
-          
-          lastCommitSizeChecks.forEach(function(binarySize) {
-            binaryMetrics.push(JSON.stringify(formatBinaryMetric(binarySize)))
-          });
-
-          var payload = binaryMetrics.join("\n")
-
-          var params = {
-              Body: zlib.gzipSync(binaryMetrics),
-              Bucket: 'mapbox-loading-dock',
-              Key: `raw/mobile_tmp.binary_size/${process.env['CIRCLE_SHA1']}.json`,
-              CacheControl: 'max-age=300',
-              ContentEncoding: 'gzip',
-              ContentType: 'application/json'
-          };
-          
-          return new AWS.S3({region: 'us-east-1'}).putObject(params, function (err, res) {
-            if (err) {
-              console.log("Error sending publishing metrics: ", err);
-            } else {
-              console.log("Binary size logged to S3 successfully")
-            }
-          });
+            return new AWS.S3({region: 'us-east-1'}).putObject({
+                Body: zlib.gzipSync(JSON.stringify(rows.reverse())),
+                Bucket: 'mapbox',
+                Key: 'mapbox-gl-native/metrics/binary-size/data.json',
+                ACL: 'public-read',
+                CacheControl: 'max-age=300',
+                ContentEncoding: 'gzip',
+                ContentType: 'application/json'
+            }).promise();
         }
     });
-}
-
-function createChartSourceData(commit) {
-  const runs = commit.checkSuites.nodes[0].checkRuns.nodes;
-  const row = [`${commit.oid.slice(0, 7)} - ${commit.messageHeadline}`];
-  
-  for (let i = 0; i < platforms.length; i++) {
-      const {platform, arch} = platforms[i];
-
-      const run = runs.find((run) => {
-          const [, p, a] = run.name.match(/Size - (\w+) ([\w-]+)/);
-          return platform === p && arch === a;
-      });
-
-      row[i + 1] = run ? +run.summary.match(/is (\d+) bytes/)[1] : undefined;
-  }
-  rows.push(row);
-}
-
-function formatBinaryMetric(item) {
-  var platform = item["name"].includes("iOS") ? 'iOS' : 'Android';
-  var arch;
-  var size = item["title"].replace(/ MB/g,'');
-
-  switch(true) {
-    case item["name"].includes("arm-v7") || item["name"].includes("armv7"):
-      arch = "arm-v7";
-      break;
-    case item["name"].includes("arm-v8"):
-      arch = "arm-v8";
-      break;
-    case item["name"].includes("x86"):
-      arch = "x86";
-      break;
-    case item["name"].includes("x86_64"):
-      arch = "x86_64";
-      break;
-    case item["name"].includes("AAR"):
-      arch = "AAR";
-      break;
-    case item["name"].includes("arm64"):
-      arch = "arm64";
-      break;
-    case item["name"].includes("Dynamic"):
-      arch = "Dynamic";
-      break;
-  }
-  
-  return {
-      'sdk': 'maps',
-      'platform' : platform,
-      'arch': arch,
-      'size' : size,
-      'commit': `${process.env['CIRCLE_SHA1']}`,
-      'created_at': `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
-  };
 }
 
 github.apps.createInstallationToken({installation_id: SIZE_CHECK_APP_INSTALLATION_ID})
